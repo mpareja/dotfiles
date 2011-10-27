ed6c539ef08d8ee8334b3bdab8a782738f86d0d4
Mario Pareja <pareja.mario@gmail.com>
2011-10-27 12:09:59 -0400

    Added basic git configuration.

---
+++ configure_git_defaults.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git config --global user.name "Mario Pareja"
+git config --global user.email "pareja.mario@gmail.com"
+git config --global core.autocrlf false
+git config --global core.editor gvim
+git config --global core.pager "less -q -x4"
+git config --global color.ui auto
+git config --global log.date relative
