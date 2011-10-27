911784ac910a67a9b290ab99cbd56c6e6f1b851c
Mario Pareja <pareja.mario@gmail.com>
2011-10-27 12:18:43 -0400

    Setting up DiffMerge as the external difftool.

--- configure_git_defaults.sh
+++ configure_git_defaults.sh
@@ -6,3 +6,10 @@ git config --global core.editor gvim
 git config --global core.pager "less -q -x4"
 git config --global color.ui auto
 git config --global log.date relative
+
+if [ -e /c ]; then
+  echo "Using DiffMerge for windows"
+  git config --global diff.tool diffmerge
+  git config --global difftool.prompt false
+  git config --global difftool.diffmerge.cmd 'c:/profiles/mpareja/Desktop/Dropbox/command_line_tools/gitextdiff.sh "$LOCAL" "$REMOTE"'
+fi
