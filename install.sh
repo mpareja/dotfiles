483591e408f1e4868c484c0f7a9357a46767af3d
Mario Pareja <pareja.mario@gmail.com>
2012-06-04 00:29:56 -0400

    Using solarized colours in gnome terminal.

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 DotFiles=$(pwd)
 
 echo Creating default ~/.bashrc
-echo ". $DotFiles/bashrc_win" > ~/.bashrc
+if [ -e /c ]; then
+	# windows
+	echo ". $DotFiles/bashrc_win" > ~/.bashrc
+else
+	echo ". $DotFiles/bashrc" > ~/.bashrc
+	$DotFiles/gnome/solarized.sh dark
+fi
 
 ./configure_git_defaults.sh
