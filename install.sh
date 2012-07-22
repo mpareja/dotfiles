1d23274dd03eef6abfa0e707da6712c1e7f3805c
Mario Pareja <pareja.mario@gmail.com>
2012-07-22 10:18:22 -0400

    Added solarized tmux colours.

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ if [ -e /c ]; then
 	# windows
 	echo ". $DotFiles/bashrc_win" > ~/.bashrc
 else
-	echo ". $DotFiles/bashrc" > ~/.bashrc
+	ln $DotFiles/bashrc ~/.bashrc
 	$DotFiles/gnome/solarized.sh dark
+
+	ln $DotFiles/tmuxcolors.conf ~/.tmuxcolors.conf
 fi
 
 ./configure_git_defaults.sh
