102e58dcde5506566ff7d608d10de248c7048803
Mario Pareja <pareja.mario@gmail.com>
2012-12-12 00:38:05 -0500

    Add installing of tmux settings.

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 DotFiles=$(pwd)
 
+replace () {
+	target="$HOME/.$1"
+	[ -e $target ] && rm $target
+	ln "$DotFiles/$1" $target
+}
+
 echo Creating default ~/.bashrc
 if [ -e /c ]; then
 	# windows
 	echo ". $DotFiles/bashrc_win" > ~/.bashrc
 else
-	ln $DotFiles/bashrc ~/.bashrc
+	echo ". $DotFiles/bashrc" > ~/.bashrc
 	$DotFiles/gnome/solarized.sh dark
 
-	ln $DotFiles/tmuxcolors.conf ~/.tmuxcolors.conf
+	replace tmux.conf
+	replace tmuxcolors.conf
 fi
 
 ./configure_git_defaults.sh
