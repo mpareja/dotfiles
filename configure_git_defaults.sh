1803a36164473e6e6f682c23537ba8d8f85755b6
Mario Pareja <pareja.mario@gmail.com>
2013-01-07 09:47:18 -0500

    Using more generic name for editor tool.

--- configure_git_defaults.sh
+++ configure_git_defaults.sh
@@ -14,8 +14,8 @@ git config --global alias.ca "commit --amend -C HEAD"
 # Borrowed from: http://www.jukie.net/bart/blog/pimping-out-git-log
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold cyan)<%an>%Creset' --abbrev-commit --date=relative"
 
-git config --global guitool.gvim.cmd 'gvim "$FILENAME"'
-git config --global guitool.gvim.noconsole yes
+git config --global guitool."edit file".cmd 'gvim "$FILENAME"'
+git config --global guitool."edit file".noconsole yes
 git config --global guitool."delete file".cmd 'rm $FILENAME'
 git config --global guitool."delete file".noconsole yes
 git config --global guitool."delete file".confirm yes
