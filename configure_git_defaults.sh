fd2293ae5ababdab85de02e788be978ec154f968
Mario Pareja <pareja.mario@gmail.com>
2013-02-05 11:53:03 -0500

    Add pull/push to GUI and clean-up tool names.

--- configure_git_defaults.sh
+++ configure_git_defaults.sh
@@ -15,15 +15,19 @@ git config --global alias.ca "commit --amend -C HEAD"
 # Borrowed from: http://www.jukie.net/bart/blog/pimping-out-git-log
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold cyan)<%an>%Creset' --abbrev-commit --date=relative"
 
-git config --global guitool."edit file".cmd 'gvim "$FILENAME"'
-git config --global guitool."edit file".noconsole yes
-git config --global guitool."delete file".cmd 'rm $FILENAME'
-git config --global guitool."delete file".noconsole yes
-git config --global guitool."delete file".confirm yes
-git config --global guitool.difftool.cmd 'git difftool -- "$FILENAME"'
-git config --global guitool.difftool.noconsole yes
-git config --global guitool."difftool staged".cmd 'git difftool --staged -- "$FILENAME"'
-git config --global guitool."difftool staged".noconsole yes
+git config --global guitool."Delete File...".cmd 'rm $FILENAME'
+git config --global guitool."Delete File...".noconsole yes
+git config --global guitool."Delete File...".confirm yes
+git config --global guitool."Pull Latest Changes from GitHub".cmd "$DOTFILES/bin/gpull"
+git config --global guitool."Pull Latest Changes from GitHub".confirm no
+git config --global guitool."Push Committed Changes to GitHub".cmd "$DOTFILES/bin/gpush"
+git config --global guitool."Push Committed Changes to GitHub".confirm yes
+git config --global guitool."Open File in Editor".cmd 'gvim "$FILENAME"'
+git config --global guitool."Open File in Editor".noconsole yes
+git config --global guitool."View Changes Externally".cmd 'git difftool -- "$FILENAME"'
+git config --global guitool."View Changes Externally".noconsole yes
+git config --global guitool."View Staged Changes Externally".cmd 'git difftool --staged -- "$FILENAME"'
+git config --global guitool."View Staged Changes Externally".noconsole yes
 
 if [ -e /c ]; then
   echo "Using DiffMerge for windows"
