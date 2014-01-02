5e2f51408014d9a58c9e0a105a642c8bedae5e9b
Mario Pareja <pareja.mario@gmail.com>
2014-01-02 10:13:54 -0500

    Extract functions from install.sh

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -3,6 +3,7 @@
 echo ======= Downloading installation scripts...
 cd $(mktemp -d)
 wget -q https://raw.github.com/mpareja/dotfiles/master/ubuntu/config
+wget -q https://raw.github.com/mpareja/dotfiles/master/ubuntu/functions
 wget -q https://raw.github.com/mpareja/dotfiles/master/ubuntu/install.sh
 wget -q https://raw.github.com/mpareja/dotfiles/master/ubuntu/install_packages.sh
 wget -q https://raw.github.com/mpareja/dotfiles/master/ubuntu/packages
--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
+. ./functions
 . ./config
+
+header Installing and configuring machine...
 
 sudo ./install_packages.sh
 
-echo ======= Installing Google Chrome...
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
-software-center /tmp/google-chrome-stable_current_amd64.deb
-
-echo ======= Configure GitHub SSH key...
-mkdir -p ~/.ssh
-cd ~/.ssh
-ssh-keygen -t rsa -C "$GIT_EMAIL" -f ./id_rsa
-echo Adding key to GitHub account
-curl -u $GIT_USERNAME https://api.github.com/user/keys -H 'Content-Type: application/json' -d "{\"title\": \"$(hostname)\",\"key\": \"$(cat id_rsa.pub)\"}"
-
-echo ======= Configure dotfiles...
-mkdir -p ~/projects
-[ -d ~/projects/dotfiles ] && rm -rI ~/projects/dotfiles
-git clone git@github.com:$GIT_USERNAME/dotfiles.git ~/projects/dotfiles
-cd ~/projects/dotfiles
-./install.sh
-
-echo ======= About to recreate ~/.vim directory...
-[ -d ~/.vim ] && rm -rI ~/.vim
-git clone git@github.com:$GIT_USERNAME/vim-settings.git ~/.vim
-cd ~/.vim
-./install.sh
-
-
+chrome
+github
+dotfiles
+vim
 
