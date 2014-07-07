bbdd11bb40bc3dbfe1e34e842d31790e413d4683
Mario Pareja <pareja.mario@gmail.com>
2014-07-07 12:35:30 -0400

    Ubuntu: install VirtualBox repository key.

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -11,6 +11,9 @@ add-apt-repository ppa:synapse-core/testing # SYNAPSE (not found in Ubuntu 14.04
 # add dropbox GPG key
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 
+# add VirtualBox GPG key
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | apt-key add -
+
 echo ======= Installing packages...
 apt-get update
 xargs apt-get install -y < packages
