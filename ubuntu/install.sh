a2a87a5c8a9673c7b77f303ef757df8b01ab3324
Mario Pareja <pareja.mario@gmail.com>
2019-08-11 21:11:05 -0400

    ubuntu: update installation scripts
    + make dropbox optional install
    + only add apt repositories if they are to be installed (virtualbox, dropb
    + ask about optional dependencies up front
    + remove skype
    + remove plank
    + remove unity tweak tool
    + remove indicator-multiload
    ox, dbeaver)
    + temporarily remove shutter

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -3,6 +3,12 @@
 . ./functions
 . ./config
 
+read -n 1 -p 'Do you want to install Dropbox (y/N)? ' INST_DROPBOX
+read -n 1 -p 'Do you want to install VirtualBox (y/N)? ' INST_VBOX
+read -n 1 -p 'Do you want to install dbeaver SQL client (y/N)? ' INST_DBEAVER
+
+echo
+
 header Installing and configuring machine...
 
 sudo ./install_packages.sh
@@ -11,13 +17,10 @@ chrome
 github
 dotfiles
 vim
-plank
 defaultapps
 docker
 install_node # I suspect needs to be after `dotfiles` due to bash profile updates
 
-read -n 1 -p 'Do you want to install VirtualBox (y/N)? ' CONFIRM
-[ $CONFIRM = 'y' ] && echo && sudo virtualbox
-
-read -n 1 -p 'Do you want to install dbeaver SQL client (y/N)? ' CONFIRM
-[ $CONFIRM = 'y' ] && echo && sudo dbeaver
+[ "$INST_DROPBOX" == 'y' ] && echo && dropbox
+[ "$INST_VBOX" == 'y' ] && echo && virtualbox
+[ "$INST_DBEAVER" = 'y' ] && echo && dbeaver
