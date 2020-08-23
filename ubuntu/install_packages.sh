5b5a408853f4c1eb10069862ad1f83259f3c97f8
Mario Pareja <pareja.mario@gmail.com>
2020-08-23 06:03:01 -0400

    ubuntu: update i3 installation approach

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo adding i3wm keyring required to avoid insecure repository warnings...
-/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb /tmp/keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
-dpkg -i /tmp/keyring.deb
+i3keyring=$(mktemp)
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb $i3keyring SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
+dpkg -i $i3keyring
 
 echo ======= Adding external repositories...
-add-apt-repository "deb http://debian.sur5r.net/i3/ $(lsb_release -sc) universe" # i3wm
-add-apt-repository -y ppa:gwendal-lebihan-dev/hexchat-stable # HEXCHAT
-add-apt-repository -y ppa:synapse-core/testing # SYNAPSE (not found in Ubuntu 14.04)
+echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" > /etc/apt/sources.list.d/sur5r-i3.list
+
 
 echo ======= Installing packages...
 apt-get update
