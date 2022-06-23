fa013e738a004c8c4737bcce3f44baeab39490bc
Mario Pareja <pareja.mario@gmail.com>
2022-06-23 16:23:00 -0400

    ubuntu: update to use regolith desktop APT repository

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo ======= Adding Regolith PPA
-add-apt-repository ppa:regolith-linux/release
+if [ ! -f /etc/apt/sources.list.d/regolith.list ]; then
+  wget -qO - https://regolith-desktop.io/regolith.key | gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg
+  echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] https://regolith-desktop.io/release-ubuntu-jammy-amd64 jammy main" | \
+    sudo tee /etc/apt/sources.list.d/regolith.list
+fi
 
 echo ======= Installing packages...
 apt-get update
